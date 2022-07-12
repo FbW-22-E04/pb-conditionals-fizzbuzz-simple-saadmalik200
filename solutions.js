5d74f905fdc2ab1484096eefe9fe32b12d209e2b
Saad Malik <saad_malik200@hotmail.com>
2022-07-12 12:45:28 +0200

    ex1

---
+++ solutions.js
@@ -0,0 +1,10 @@
+let num = 15;
+if (num % 3 == 0 && num % 5 !== 0) {
+  console.log("Fizz");
+} else if (num % 5 == 0 && num % 3 !== 0) {
+  console.log("Buzz");
+} else if (num % 3 == 0 && num % 5 == 0) {
+  console.log("FizzBuzz");
+} else {
+  console.log(num);
+}
